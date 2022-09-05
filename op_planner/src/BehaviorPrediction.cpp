56fa1f98d64119e497d25bc1f2fe9c8a4ef0aded
mahir <gulzar@ut.ee>
2022-09-05 12:49:57 +0300

    [#386] Added closest waypoints size check before taking nearest lane direction

--- op_planner/src/BehaviorPrediction.cpp
+++ op_planner/src/BehaviorPrediction.cpp
@@ -181,7 +181,7 @@ void BehaviorPrediction::PredictCurrentTrajectory(RoadNetwork& map, ObjParticles
 	// ------------------------------
 	// Speed Sign Estimation
 	// ------------------------------
-	if (m_bsignEstimation)
+	if (m_bsignEstimation && pCarPart->obj.pClosestWaypoints.size() > 0)
 	{	
 		// Take first waypoint from the closest waypoints list.
 
